c70cc2ee46e9be1140054e0071456bdc64e38f51
honeymon <ihoneymon@gmail.com>
2016-08-07 16:03:38 +0900

    Initialize Project: Boot Spring Boot

---
+++ src/main/java/io/honeymon/springboot/boot/BootSpringBootApplication.java
@@ -0,0 +1,12 @@
+package io.honeymon.springboot.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringBootApplication.class, args);
+	}
+}
