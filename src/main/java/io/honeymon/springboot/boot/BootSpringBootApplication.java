fc09b4be5e77fbdb9a36d085182d879ca973a35b
honeymon <ihoneymon@gmail.com>
2017-06-03 12:40:21 +0900

    SpringBoot upgrade: 1.4.0 -> 1.5.3

--- src/main/java/io/honeymon/springboot/boot/BootSpringBootApplication.java
+++ src/main/java/io/honeymon/springboot/boot/BootSpringBootApplication.java
@@ -2,11 +2,15 @@ package io.honeymon.springboot.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BootSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootSpringBootApplication.class, args);
+		SpringApplication app = new SpringApplication(BootSpringBootApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
+	
 }
