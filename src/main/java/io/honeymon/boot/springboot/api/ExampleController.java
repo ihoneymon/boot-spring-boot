c7209d35f0d80ab45559e3f544385f030f0c88b3
honeymon <ihoneymon@gmail.com>
2017-06-06 15:33:18 +0900

    EventListener 이름 수정

--- src/main/java/io/honeymon/boot/springboot/api/ExampleController.java
+++ src/main/java/io/honeymon/boot/springboot/api/ExampleController.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PropertySource(value = { "" })
 @RestController
 public class ExampleController {
 
