2615869c28395494c01c003d2144b3b5656c9fe5
honeymon <ihoneymon@gmail.com>
2017-11-28 13:02:58 +0900

    add error.html

---
+++ src/main/java/io/honeymon/boot/springboot/view/handler/ViewExceptionHandler.java
@@ -0,0 +1,29 @@
+package io.honeymon.boot.springboot.view.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import io.honeymon.boot.springboot.view.annotation.ViewController;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * {@link ViewController} 에 대한 예외처리
+ * @author honeymon
+ *
+ */
+@Slf4j
+@ControllerAdvice(annotations= {ViewController.class})
+public class ViewExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultExceptionHandler(HttpServletRequest req, Exception e) {
+        log.error("Occurred Exception: {}", e.getMessage());
+        ModelAndView mav = new ModelAndView("error");
+        mav.addObject("e", e);
+        mav.addObject("url", req.getRequestURL());
+        return mav;
+    }
+}
