259f0c827ff508a758ec855d26dec35a6b2d01b9
honeymon <ihoneymon@gmail.com>
2018-02-01 14:24:01 +0900

    Spring Boot Upgrade: 2.0.0.M7 -> 2.0.0.RC1

--- src/main/java/AvoidDefaultPackagePosition.java
+++ src/main/java/AvoidDefaultPackagePosition.java
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 이 위치에 생성하는 것은 피하자!!
  * 
  * @author honeymon
- * @see <a href="http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#using-boot-using-the-default-package">Using the “default” package</a>
- * @see SpringBootApplication 이 선언되어 있는 메인클래스의 위치에 따라 관련된 컴포넌트를 탐색하는데 사용된다.
+ * 
+ *         <a href=
+ *         "http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#using-boot-using-the-default-package">Using
+ *         the “default” package</a> SpringBootApplication 이 선언되어 있는 메인클래스의 위치에
+ *         따라 관련된 컴포넌트를 탐색하는데 사용된다.
  */
 public class AvoidDefaultPackagePosition {
 }
