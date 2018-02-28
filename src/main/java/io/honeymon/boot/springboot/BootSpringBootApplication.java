de7cc45a5c48ae72c7818f48c89fcc6bc191cb03
honeymon <ihoneymon@gmail.com>
2018-02-28 16:44:15 +0900

    Spring Boot upgrade: 2.0.0.RC1 -> 2.0.0.RC2

--- src/main/java/io/honeymon/boot/springboot/BootSpringBootApplication.java
+++ src/main/java/io/honeymon/boot/springboot/BootSpringBootApplication.java
@@ -2,7 +2,7 @@ package io.honeymon.boot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
