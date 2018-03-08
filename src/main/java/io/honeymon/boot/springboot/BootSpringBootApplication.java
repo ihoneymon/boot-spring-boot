bca3fb5b2286ac9e1c259a268da5b4516ab03fa3
honeymon <ihoneymon@gmail.com>
2018-03-08 17:15:59 +0900

    변경: `setWebEnvironment` -> `setWebApplicationType`

--- src/main/java/io/honeymon/boot/springboot/BootSpringBootApplication.java
+++ src/main/java/io/honeymon/boot/springboot/BootSpringBootApplication.java
@@ -1,6 +1,7 @@
 package io.honeymon.boot.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,7 +9,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 /**
  * 스프링 부트 애플리케이션이 시작되는 곳!
  * {@link SpringBootApplication}을 살펴보세요. 스프링 부투의 마법이 시작되는 곳입니다.
- * 
+ *
  * @author honeymon
  *
  */
@@ -20,6 +21,8 @@ public class BootSpringBootApplication {
 
 		SpringApplication app = new SpringApplication(BootSpringBootApplication.class);
 		app.addListeners(new ApplicationPidFileWriter());
+		//app.setWebEnvironment(false);	// 2.0 에서 Deprecated 됨
+		app.setWebApplicationType(WebApplicationType.SERVLET);
 		app.run(args);
 	}
 }
