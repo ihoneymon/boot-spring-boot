ecd13833bf94fe91cad19b612d9567d3863121c5
Honeymon <ihoneymon@gmail.com>
2020-12-26 00:12:13 +0900

    Spring Boot 2.4.1 업그레이드

--- src/test/java/io/honeymon/boot/springboot/BootSpringBootApplicationTests.java
+++ src/test/java/io/honeymon/boot/springboot/BootSpringBootApplicationTests.java
@@ -1,11 +1,12 @@
 package io.honeymon.boot.springboot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+/**
+ * Spring Boot 2.4.0 부터 junit-vintage-engine 이 제거되었다.
+ * Spring Boot 2.4.0 으로 업그레이드 하려면 JUnit5 로 이관을 마쳐야 한다.
+ */
 @SpringBootTest
 public class BootSpringBootApplicationTests {
 
--- src/test/java/io/honeymon/boot/springboot/domain/UserRepositoryTest.java
+++ src/test/java/io/honeymon/boot/springboot/domain/UserRepositoryTest.java
@@ -1,16 +1,11 @@
 package io.honeymon.boot.springboot.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class UserRepositoryTest {
 
