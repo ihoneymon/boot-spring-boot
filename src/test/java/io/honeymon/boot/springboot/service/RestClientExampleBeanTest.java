4ce6f65684343c014499ea1c9746d9bb5906ff9a
Honeymon <ihoneymon@gmail.com>
2020-12-26 00:12:13 +0900

    Spring Boot 2.4.1 업그레이드

--- src/test/java/io/honeymon/boot/springboot/service/RestClientExampleBeanTest.java
+++ src/test/java/io/honeymon/boot/springboot/service/RestClientExampleBeanTest.java
@@ -1,18 +1,24 @@
 package io.honeymon.boot.springboot.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import io.honeymon.boot.springboot.service.RestClientExampleBean.Post;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import io.honeymon.boot.springboot.service.RestClientExampleBean.Post;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+/**
+ * ExtendWith는 JUnit5 에서 반복적으로 실행되는 클래스나 메서드에 선언한다.
+ * {@link SpringExtension} 클래스는 는 스프링 5에 추가된
+ * JUnit 5의 주피터(Jupiter) 모델에서 스프링 테스트컨텍스트(TestContext)를 사용할 수 있도록 해준다.
+ *
+ * 출처: https://java.ihoney.pe.kr/525 [허니몬(Honeymon)의 자바guru]
+ *
+ * https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-testing-spring-boot-applications
+ */
+//@ExtendWith(SpringExtension.class) // JUnit5 와 스프링 부트를 사용한다면 @SpringBootTest 로 대체 가능
 @SpringBootTest
 public class RestClientExampleBeanTest {
 
